c8b299fb9d7f7a72242e6f167cb61c28e711b85a
Vinicius Franca <vinilima07@gmail.com>
2019-10-24 11:52:53 -0300

    up database script and model

--- database.sql
+++ database.sql
@@ -1,9 +1,108 @@
-DROP TABLE IF EXISTS endereco;
-DROP SEQUENCE IF EXISTS endereco_sequence;
-CREATE SEQUENCE endereco_sequence;
-CREATE TABLE endereco (
-  idendereco int NOT NULL PRIMARY KEY DEFAULT nextval('endereco_sequence'),
-  bairro varchar(100) NOT NULL
-);
+-- Database generated with pgModeler (PostgreSQL Database Modeler).
+-- pgModeler  version: 0.9.1-beta
+-- PostgreSQL version: 10.0
+-- Project Site: pgmodeler.com.br
+-- Model Author: ---
 
-INSERT INTO endereco VALUES (1,'Barreiro'),(2,'Barro Preto');
+
+-- Database creation must be done outside an multicommand file.
+-- These commands were put in this file only for convenience.
+-- -- object: new_database | type: DATABASE --
+-- -- DROP DATABASE IF EXISTS new_database;
+-- CREATE DATABASE new_database
+-- ;
+-- -- ddl-end --
+-- 
+
+-- object: public.imovel | type: TABLE --
+-- DROP TABLE IF EXISTS public.imovel CASCADE;
+CREATE TABLE public.imovel(
+	b_armario boolean DEFAULT FALSE,
+	n_quartos smallint,
+	n_suites smallint,
+	area double precision,
+	n_salas_estar smallint,
+	n_vagas_garagem smallint,
+	id_imovel serial NOT NULL,
+	descricao varchar,
+	id_endereco integer,
+	CONSTRAINT pk_imovel PRIMARY KEY (id_imovel)
+
+);
+-- ddl-end --
+ALTER TABLE public.imovel OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.apartamento | type: TABLE --
+-- DROP TABLE IF EXISTS public.apartamento CASCADE;
+CREATE TABLE public.apartamento(
+	id_apartamento serial NOT NULL,
+	n_andar smallint,
+	valor_condominio double precision DEFAULT 0,
+	b_portaria boolean DEFAULT FALSE,
+	n_salas_jantar smallint,
+	id_imovel integer NOT NULL,
+	CONSTRAINT pk_apartamento PRIMARY KEY (id_apartamento)
+
+);
+-- ddl-end --
+ALTER TABLE public.apartamento OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.casa | type: TABLE --
+-- DROP TABLE IF EXISTS public.casa CASCADE;
+CREATE TABLE public.casa(
+	id_casa serial NOT NULL,
+	id_imovel integer NOT NULL
+);
+-- ddl-end --
+ALTER TABLE public.casa OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.endereco | type: TABLE --
+-- DROP TABLE IF EXISTS public.endereco CASCADE;
+CREATE TABLE public.endereco(
+	id_endereco serial NOT NULL,
+	numero smallint,
+	rua varchar,
+	cidade varchar,
+	bairro varchar,
+	CONSTRAINT id_endereco PRIMARY KEY (id_endereco)
+
+);
+-- ddl-end --
+ALTER TABLE public.endereco OWNER TO postgres;
+-- ddl-end --
+
+-- object: imovel_fk | type: CONSTRAINT --
+-- ALTER TABLE public.apartamento DROP CONSTRAINT IF EXISTS imovel_fk CASCADE;
+ALTER TABLE public.apartamento ADD CONSTRAINT imovel_fk FOREIGN KEY (id_imovel)
+REFERENCES public.imovel (id_imovel) MATCH FULL
+ON DELETE RESTRICT ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: apartamento_uq | type: CONSTRAINT --
+-- ALTER TABLE public.apartamento DROP CONSTRAINT IF EXISTS apartamento_uq CASCADE;
+ALTER TABLE public.apartamento ADD CONSTRAINT apartamento_uq UNIQUE (id_imovel);
+-- ddl-end --
+
+-- object: imovel_fk | type: CONSTRAINT --
+-- ALTER TABLE public.casa DROP CONSTRAINT IF EXISTS imovel_fk CASCADE;
+ALTER TABLE public.casa ADD CONSTRAINT imovel_fk FOREIGN KEY (id_imovel)
+REFERENCES public.imovel (id_imovel) MATCH FULL
+ON DELETE RESTRICT ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: casa_uq | type: CONSTRAINT --
+-- ALTER TABLE public.casa DROP CONSTRAINT IF EXISTS casa_uq CASCADE;
+ALTER TABLE public.casa ADD CONSTRAINT casa_uq UNIQUE (id_imovel);
+-- ddl-end --
+
+-- object: endereco_fk | type: CONSTRAINT --
+-- ALTER TABLE public.imovel DROP CONSTRAINT IF EXISTS endereco_fk CASCADE;
+ALTER TABLE public.imovel ADD CONSTRAINT endereco_fk FOREIGN KEY (id_endereco)
+REFERENCES public.endereco (id_endereco) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+-- ddl-end --
+
+
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/malvgomes/gwtTrabalho/venv"
+set -gx VIRTUAL_ENV "/home/vinicius/Desktop/faculdade/gwtTrabalho/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
